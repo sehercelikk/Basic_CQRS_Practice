b8a634703c7869aca3baa933f72aed94ceb573bc
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-11-20 14:29:07 +0300

    Added Persistence Layer

---
+++ RentACar/corePackages/Core.Packages/rentACar/RentACar/Persistence/PersistenceServiceRegistiration.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistiration
+{
+    public static IServiceCollection AddPersistenceService(this IServiceCollection service, IConfiguration configuration)
+    {
+        service.AddDbContext<BaseDbContext>(opt=>
+        {
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
+        });
+
+        service.AddScoped<IBrandReposiory, BrandRepository>();
+        return service;
+    }
+}
---
+++ RentACar/corePackages/Core.Packages/rentACar/RentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class BrandRepository : EfRepositoryBase<Brand, Guid, BaseDbContext>, IBrandReposiory
+{
+    public BrandRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
