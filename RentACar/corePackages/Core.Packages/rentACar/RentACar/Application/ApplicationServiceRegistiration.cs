cdbf855684b54aa07e9e43a11029b7fe87a25b0f
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-11-20 14:27:45 +0300

    Added Application Layer

---
+++ RentACar/corePackages/Core.Packages/rentACar/RentACar/Application/ApplicationServiceRegistiration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class ApplicationServiceRegistiration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()); // Git bütün assambly i tara orda commendleri queryleri bul onları handlerları ile eşle ben sana commandSend yaparsam çalıştır.
+        });
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
---
+++ RentACar/corePackages/Core.Packages/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Brands.Commands.Create;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<CreateBrandCommand, Brand>().ReverseMap();
+        CreateMap<CreatedBrandResponse,Brand >().ReverseMap();
+    }
+}
